46177621217232b15a00e4ba907c3d7982bc2b2c
oliyo <oliyo2023@gmail.com>
2025-03-26 20:36:37 +0800

    refactor: 移除不必要的 TimeService 导入和初始化逻辑

--- lib/pages/clock/clock_page.dart
+++ lib/pages/clock/clock_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
 import '../../utils/chinese_calendar.dart';
-import 'package:get/get.dart'; // 导入 Get
-import '../../services/time_service.dart'; // 导入 TimeService
+// 导入 Get
+// 导入 TimeService
 
 class ClockPage extends StatefulWidget {
   const ClockPage({super.key});
@@ -16,18 +16,9 @@ class ClockPage extends StatefulWidget {
 class _ClockPageState extends State<ClockPage> {
   late Timer _timer;
   DateTime _currentTime = DateTime.now();
-  final TimeService _timeService = Get.find<TimeService>(); // 获取 TimeService 实例
+  // 获取 TimeService 实例
 
   @override
-    super.initState();
-    // 使用校准后的时间初始化
-    _currentTime = _timeService.getCorrectedTime();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        // 使用校准后的时间更新
-        _currentTime = _timeService.getCorrectedTime();
-      });
-    });
   void initState() {
     super.initState();
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
